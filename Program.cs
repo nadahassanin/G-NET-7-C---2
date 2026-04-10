898b141d181789612f1ae1a279c72f4ac7c696a9
DESKTOP-T7PH5A9\nada <nadahassanin189@gmail.com>
2026-04-10 17:05:22 +0200

    Question1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp7
+﻿using static System.Reflection.Metadata.BlobBuilder;
+using System;
+
+namespace ConsoleApp7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+           
+
+
+
+
+            #region
+            // Q: What is the purpose of #region and #endregion directives?
+            /* A: They are used to organize code by allowing developers to collapse or expand
+            specific blocks of code in the IDE This makes large files more readable*/
+
+
+            #endregion
+
+
+
+
+
+
         }
     }
 }
